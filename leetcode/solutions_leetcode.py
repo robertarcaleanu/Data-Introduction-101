2efe87e509cd6e7f7870eda5507837ee86bc2bfb
Robert Arcaleanu <alumnerobert@gmail.com>
2024-09-12 20:48:05 +0200

    Added new solutions

--- leetcode/solutions_leetcode.py
+++ leetcode/solutions_leetcode.py
@@ -17,3 +17,15 @@ class SolutionsLeetcode:
         df = self.datasets.big_countries()
 
         return df[(df["area"] >= 3e6) | (df["population"] >= 25000000)]
+
+    def find_products(self) -> pd.DataFrame:
+        df = self.datasets.recyclable_and_low_fat_products()
+
+        return df[(df["low_fats"] == "Y") & (df["recyclable"] == "Y")][["product_id"]]
+    
+    def article_views(self) -> pd.DataFrame:
+        customers, orders = self.datasets.customers_who_never_order()
+        solution = customers[~customers["id"].isin(orders["customer_id"])][["name"]]
+        solution = solution.rename(columns={"name": "Customers"})
+
+        return solution
