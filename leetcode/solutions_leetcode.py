865af2140960ab37dc04fbc9a3c852f3d5e756b2
Robert Arcaleanu <alumnerobert@gmail.com>
2024-09-16 21:25:04 +0200

    Added more solutions

--- leetcode/solutions_leetcode.py
+++ leetcode/solutions_leetcode.py
@@ -24,8 +24,37 @@ class SolutionsLeetcode:
         return df[(df["low_fats"] == "Y") & (df["recyclable"] == "Y")][["product_id"]]
     
     def article_views(self) -> pd.DataFrame:
-        customers, orders = self.datasets.customers_who_never_order()
-        solution = customers[~customers["id"].isin(orders["customer_id"])][["name"]]
-        solution = solution.rename(columns={"name": "Customers"})
+        df = self.datasets.article_views_1()
+        df = df[df["author_id"]  == df["viewer_id"]]
+        df = df.drop_duplicates(subset=["author_id"])
 
-        return solution
+        return df
+    def invalid_tweets(self) -> pd.DataFrame:
+        df = self.datasets.invalid_tweets()
+        df["tweet_len"] = df[df["content"].str.len()]
+        df = df[df["tweet_len"] > 15][["tweet_id"]]
+
+        return df
+    
+    def calculate_special_bonus(self) -> pd.DataFrame:
+        df = self.datasets.calculate_special_bonus()
+        df["id_is_odd"] = df["employee_id"] % 2  # Create s column - Check if number is odd (0 is even, 1 is odd)
+        df["name_starts_with_m"] = df["name"].str.startswith("M") #  Create s column - Check if name starts with M
+        df.loc[(df["id_is_odd"] == True) & (df["name_starts_with_m"] == False), "bonus"] = df["salary"] # if number is odd and name does not starts with M, we create a column bonus which equal to the salary
+        df["bonus"] = df["bonus"].fillna(0)  # fill NaN values with 0
+
+        return df
+    
+    def fix_names(self) -> pd.DataFrame:
+        df = self.datasets.fix_names_in_table()
+        df["name"] = df["name"].str.capitalize()
+        
+        return df
+    
+    def valid_email(self) -> pd.DataFrame:
+        pass
+
+    def find_patients(self) -> pd.DataFrame:
+        pass
+
+
